76a9d212b8c9f53992580e73b0161bd66d6874fd
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-01-15 22:29:44 +0000

    Add Inventory Service

---
+++ inventory-service/src/main/java/com/atarist/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.atarist.inventoryservice.controller;
+
+import com.atarist.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{skuCode}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+
+    }
+}
